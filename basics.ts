4abdcb42cf5a6814678be8af12c999b74f64cffc
justicepelteir <justice.douglas@hey.com>
2023-05-20 13:43:30 -0700

    learn about void types and function return types

--- basics.ts
+++ basics.ts
@@ -45,3 +45,13 @@ let people: Person[];
 let course: string | number = 'React - The Complete Guide';
 
 course = 1234;
+
+// Functions & Types
+
+function add(a: number, b: number) {
+    return a + b;
+}
+
+function print(value: any) {
+    console.log(value);
+}
